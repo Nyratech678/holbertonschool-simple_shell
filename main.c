f25b00add37a97676e20a34990be42a6de3f06de
Nyratech678 <uwuchocolatemilk09876@proton.me>
2025-08-21 11:17:02 +0200

    Refactor main function: replace loop function call with inline command execution loop

--- main.c
+++ main.c
@@ -8,8 +8,72 @@
  */
 int main(int argc, char **argv)
 {
+	char *line = NULL;
+	size_t len = 0;
+	ssize_t read;
+	pid_t pid;
+	int status;
+	unsigned int count = 0;
+
 	(void)argc;
 
-	loop(argv);
+	while (1)
+	{
+		count++;
+
+		/* Display prompt only in interactive mode */
+		if (isatty(STDIN_FILENO))
+		{
+			write(STDOUT_FILENO, "($) ", 4);
+			fflush(stdout);
+		}
+
+		/* Read input */
+		read = getline(&line, &len, stdin);
+		if (read == -1)
+		{
+			if (isatty(STDIN_FILENO))
+				write(STDOUT_FILENO, "\n", 1);
+			break;
+		}
+
+		/* Remove newline */
+		if (line[read - 1] == '\n')
+			line[read - 1] = '\0';
+
+		/* Skip empty lines */
+		if (line[0] == '\0')
+			continue;
+
+		/* Handle exit */
+		if (_strcmp(line, "exit") == 0)
+			break;
+
+		/* Fork and execute */
+		pid = fork();
+		if (pid == 0)
+		{
+			/* Child process */
+			char *args[2];
+			args[0] = line;
+			args[1] = NULL;
+			if (execve(line, args, environ) == -1)
+			{
+				fprintf(stderr, "%s: %u: %s: not found\n", argv[0], count, line);
+				_exit(127);
+			}
+		}
+		else if (pid < 0)
+		{
+			perror("fork");
+		}
+		else
+		{
+			/* Parent process */
+			wait(&status);
+		}
+	}
+
+	free(line);
 	return (0);
 }
