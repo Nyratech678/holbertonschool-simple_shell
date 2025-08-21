b1580003a06d47a69ef3ff874ccccab0ac336c53
Nyratech678 <uwuchocolatemilk09876@proton.me>
2025-08-21 11:57:05 +0200

    Implement command lookup: add find_command function to locate commands in PATH and handle absolute/relative paths

--- main.c
+++ main.c
@@ -1,6 +1,63 @@
 #include "shell.h"
 
 /**
+ * find_command - Find command in PATH or return full path if exists
+ * @cmd: Command to find
+ * Return: Full path to command or NULL if not found
+ */
+char *find_command(char *cmd)
+{
+	char *path_env, *path_copy, *dir;
+	char *full_path;
+	struct stat st;
+	
+	/* If command contains '/', treat as absolute/relative path */
+	if (strchr(cmd, '/'))
+	{
+		if (stat(cmd, &st) == 0)
+			return (_strdup(cmd));
+		return (NULL);
+	}
+	
+	/* Get PATH environment variable */
+	path_env = _getenv("PATH");
+	if (!path_env)
+		return (NULL);
+		
+	path_copy = _strdup(path_env);
+	if (!path_copy)
+		return (NULL);
+		
+	/* Search in each PATH directory */
+	dir = strtok(path_copy, ":");
+	while (dir)
+	{
+		full_path = malloc(_strlen(dir) + _strlen(cmd) + 2);
+		if (!full_path)
+		{
+			free(path_copy);
+			return (NULL);
+		}
+		
+		_strcpy(full_path, dir);
+		_strcat(full_path, "/");
+		_strcat(full_path, cmd);
+		
+		if (stat(full_path, &st) == 0)
+		{
+			free(path_copy);
+			return (full_path);
+		}
+		
+		free(full_path);
+		dir = strtok(NULL, ":");
+	}
+	
+	free(path_copy);
+	return (NULL);
+}
+
+/**
  * main - Entry point of the simple shell
  * @argc: Number of arguments
  * @argv: Array of arguments
@@ -21,7 +78,7 @@ int main(int argc, char **argv)
 		/* Display prompt only in interactive mode */
 		if (isatty(STDIN_FILENO))
 		{
-			write(STDOUT_FILENO, "#cisfun$ ", 9);
+			write(STDOUT_FILENO, ":) ", 3);
 			fflush(stdout);
 		}
 
@@ -49,6 +106,7 @@ int main(int argc, char **argv)
 			char *args[1024];
 			int argc = 0;
 			char *token;
+			char *cmd_path;
 			
 			while (*trimmed == ' ' || *trimmed == '\t')
 				trimmed++;
@@ -76,25 +134,36 @@ int main(int argc, char **argv)
 			if (argc == 0)
 				continue;
 
+			/* Find command in PATH */
+			cmd_path = find_command(args[0]);
+			if (!cmd_path)
+			{
+				fprintf(stderr, "%s: No such file or directory\n", argv[0]);
+				continue;
+			}
+
 			/* Fork and execute */
 			pid = fork();
 			if (pid == 0)
 			{
 				/* Child process */
-				if (execve(args[0], args, environ) == -1)
+				if (execve(cmd_path, args, environ) == -1)
 				{
 					fprintf(stderr, "%s: No such file or directory\n", argv[0]);
+					free(cmd_path);
 					_exit(127);
 				}
 			}
 			else if (pid < 0)
 			{
 				perror("fork");
+				free(cmd_path);
 			}
 			else
 			{
 				/* Parent process */
 				wait(&status);
+				free(cmd_path);
 			}
 		}
 	}
